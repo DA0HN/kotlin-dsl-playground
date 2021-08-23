3a9592488e44600c0a6a9cee80f3468e1bbc572b
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-23 18:35:42 -0400

    wip: adiciona entidades base

---
+++ src/main/kotlin/Address.kt
@@ -0,0 +1,9 @@
+/**
+ * @author daohn
+ * @since 23/08/2021
+ */
+data class Address(
+    var street: String?,
+    var number: Int?,
+    var city: String?
+)
---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,9 @@
+/**
+ * @author daohn
+ * @since 23/08/2021
+ */
+data class Person(
+    var name: String? = null,
+    var age: Int? = null,
+    var address: Address? = null
+)
